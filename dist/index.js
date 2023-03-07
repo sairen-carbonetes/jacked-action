54b074309144ad4a69ff572f57cb608f49289c05
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-03-07 10:13:58 +0800

    update index.js

--- dist/index.js
+++ dist/index.js
@@ -2,15 +2,19 @@
 const { execSync } = require('child_process');
 
 try {
-  // Clone the carbonetes/jacked repository
-  execSync('git clone https://github.com/carbonetes/jacked.git');
+    // Clone the carbonetes/jacked repository
+    execSync('git clone https://github.com/carbonetes/jacked.git');
 
-  // Install Go 1.19
-  execSync('curl -O https://go.dev/dl/go1.19.linux-amd64.tar.gz');
-  execSync('tar -xvf go1.19.linux-amd64.tar.gz');
-  execSync('sudo mv go /usr/local');
+    // Install Go 1.19
+    // Download Go tarball
+    execSync('curl -O https://go.dev/dl/go1.19.linux-amd64.tar.gz');
 
-  console.log('Jacked repository cloned and Go installed successfully.');
+    // Extract tarball to /usr/local
+    execSync('sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz');
+
+    console.log('Go installed successfully.');
+
+    console.log('Jacked repository cloned and Go installed successfully.');
 } catch (error) {
-  console.error(`Set Workspace Error: ${error.message}`);
+    console.error(`Set Workspace Error: ${error.message}`);
 }
