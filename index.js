458d249dfc98609e9c4ff94d4a8c60bf1b89ed05
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-03-07 16:49:05 +0800

    feat: init commit npm

---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+async function run() {
+    try {
+        // Download and install the script
+        await exec.exec('curl', ['-sSfL', 'https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh', '|', 'sh', '-s', '--']);
+
+        // Call the binary
+        await exec.exec('jacked');
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
