ab6e3ecbec89a92a9bf5745281ae6431c8a833d5
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-03-07 15:05:29 +0800

    update function

--- dist/index.js
+++ dist/index.js
@@ -10,12 +10,26 @@ exec('curl -sSfL https://raw.githubusercontent.com/carbonetes/jacked/main/instal
 
 });
 
-// Call the jacked binary
-exec('jacked', (error, stdout, stderr) => {
-if (error) {
-    console.error(`exec error: ${error}`);
-    return;
+downloadBinary()
+
+async function downloadBinary() {
+    // Download binary
+   await exec('curl -sSfL https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh | sh -s --', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec curl error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+
+    // Call the jacked binary
+    await exec('jacked', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+    });
 }
-console.log(`stdout: ${stdout}`);
-console.error(`stderr: ${stderr}`);
-});
