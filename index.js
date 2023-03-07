d59f9e8f471ca7ba9e52670e2ab834c04da0b962
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-03-07 17:02:22 +0800

    update action

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
+// Get input
+const directory = core.getInput('directory', { required: true })
+
 async function run() {
     try {
+        console.log("User directory input: ", directory)
         // Download and install the script
         await exec.exec('curl', ['-sSfL', 'https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh', '|', 'sh', '-s', '--']);
 
