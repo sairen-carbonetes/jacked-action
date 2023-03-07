50c95d26f61b0a554775a6d93839f22fbe76afd2
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-03-07 10:35:09 +0800

    update download jacked binary

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,10 @@
 const { execSync } = require('child_process');
 
 try {
-    // Clone the carbonetes/jacked repository
-    execSync('git clone https://github.com/carbonetes/jacked.git');
-
-    // Install Go 1.19
-    // Download Go tarball
-    execSync('curl -O https://go.dev/dl/go1.19.linux-amd64.tar.gz');
-
-    // Extract tarball to /usr/local
-    execSync('sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz');
-
-    console.log('Go installed successfully.');
-
-    console.log('Jacked repository cloned and Go installed successfully.');
+    // download the carbonetes/jacked binary
+    execSync('curl -sSfL https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh | sh -s -- -d /usr/local/bin');
+    execSync('jacked')
+    console.log('Jacked binary installed.');
 } catch (error) {
     console.error(`Set Workspace Error: ${error.message}`);
 }
