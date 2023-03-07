405153d3dc5e3ad5c2769f6164765f71a9b6acbb
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-03-07 15:29:20 +0800

    update function

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,8 @@
 const { exec } = require('child_process');
 
-exec('curl -sSfL https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh | sh -s --', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec curl error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-
-});
-
-downloadBinary();
-callBinary();
-
-function downloadBinary() {
-    // Download binary
+async function downloadBinary() {
+    // Download Binary
+    console.log("Download Binary");
     exec('curl -sSfL https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh | sh -s --', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec curl error: ${error}`);
@@ -24,9 +12,13 @@ function downloadBinary() {
         console.error(`stderr: ${stderr}`);
     });
 }
-
-function callBinary() {
-    // Call the jacked binary
+  
+async function callBinary() {
+    // Download Binary
+    await downloadBinary();
+    
+    // Call Binary
+    console.log("Call Binary");
     exec('jacked', (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
@@ -36,3 +28,5 @@ function callBinary() {
         console.error(`stderr: ${stderr}`);
     });
 }
+  
+callBinary();
