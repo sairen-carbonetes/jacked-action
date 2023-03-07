77a27f37ae6f8612875f47f27b2daf262bf93a71
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-03-07 16:07:44 +0800

    update function

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,16 @@
-const { exec } = require('child_process');
+const core = require('@actions/core');
+const exec = require('@actions/exec');
 
-function installJacked() {
-    // Download Binary
-    console.log("Download Binary");
-    exec('curl -sSfL https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh | sh -s --', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec curl error: ${error}`);
-            return;
-        } else {
-            // Call Binary
-            exec('jacked', (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
-            });
-        }
-    });
+async function run() {
+  try {
+    // Download and install the script
+    await exec.exec('curl', ['-sSfL', 'https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh', '|', 'sh', '-s', '--']);
+
+    // Call the binary
+    await exec.exec('jacked');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-installJacked();
+run();
