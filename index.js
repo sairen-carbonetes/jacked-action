4cbb28bdd3960292cad7be5843f34016dc1f8183
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-03-07 18:07:17 +0800

    update action

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 
 const cmdExec = require('node:child_process');
-const url = "https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh"
 
 // Get input
 const directory = core.getInput('directory', { required: true })
@@ -10,15 +9,24 @@ const directory = core.getInput('directory', { required: true })
 async function run() {
     try {
         // Download and install the script
-        await exec.exec('curl', ['-sSfL', url, '|', 'sh']);
-        await exec.exec('chmod', ['+x', 'script.sh']);
-        await exec.exec('sudo', ['bash', './script.sh']);
+        const response = await fetch('https://raw.githubusercontent.com/carbonetes/jacked/main/install.sh');
+        const script = await response.text();
+
+        // Save the script to a file
+        await writeFile('./install.sh', script);
+
+        // Make the script executable
+        await exec.exec('chmod', ['+x', './install.sh']);
+
+        // Run the script with the -d option to specify the installation directory
+        await exec.exec('./install.sh', ['-d', '/usr/local/bin']);
 
         // Installation successful
-        core.info('Script has been installed');
+        core.info('Jacked has been installed');
 
         // Call the binary
         await exec.exec('jacked');
+
     } catch (error) {
         core.setFailed(error.message);
     }
